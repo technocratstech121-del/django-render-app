fd8e9f2f94ad7f33ecc94d1163b5d2c61d02bef5
technocrats <technocratstech121@gmail.com>
2025-12-24 20:11:23 +0530

    Fix build.sh for Render

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-echo "Installing dependencies..."
 pip install -r requirements.txt
-
-echo "Running migrations..."
 python manage.py migrate
-
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
